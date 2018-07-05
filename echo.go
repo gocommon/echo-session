fc05db8c843bfdafcd997df0cbbd69a0cce5ec9e
weisd <weishidavip@163.com>
2018-07-05 11:31:29 +0800

    modify from beego

---
+++ echo.go
@@ -0,0 +1,60 @@
+package session
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+var (
+	// DefaultContextKey DefaultContextKey
+	DefaultContextKey = "_CONTEXT_SESSION_KEY"
+)
+
+type (
+	// MiddlewareConfig MiddlewareConfig
+	MiddlewareConfig struct {
+		Skipper middleware.Skipper
+		Manager *Manager
+	}
+)
+
+// Middleware Middleware for echo only
+func Middleware(config MiddlewareConfig) echo.MiddlewareFunc {
+
+	if config.Manager == nil {
+		panic("MiddlewareConfig.Manager must be set")
+	}
+
+	if config.Skipper == nil {
+		config.Skipper = middleware.DefaultSkipper
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+
+			sess, err := config.Manager.SessionStart(c.Response(), c.Request())
+			if err != nil {
+				return err
+			}
+
+			c.Set(DefaultContextKey, sess)
+
+			err = next(c)
+			if err != nil {
+				return err
+			}
+
+			sess.SessionRelease(c.Response())
+
+			return nil
+		}
+	}
+}
+
+// Session Session Store from context
+func Session(c echo.Context) Store {
+	return c.Get(DefaultContextKey).(Store)
+}
